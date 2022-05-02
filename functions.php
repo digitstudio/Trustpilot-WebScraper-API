d8feb0550c87393812392b2a98156ac1c6cd4aa4
Jérôme MARICHEZ <62617561+digitstudio@users.noreply.github.com>
2022-05-02 17:57:53 +0200

    Addeed API Call
    Added some API Call who allow
    
    Show Score /5 of trustpilot (score)
    Show Stars img (scoreimg)
    Show totalreview (totalreview)
    __________________________________
    Show widgetmini (widgetmini) in Work In Progress (need to render the Widget)

--- functions.php
+++ functions.php
@@ -1,4 +1,45 @@
-<?php 
+<?php
+
+function get_score_from_trustpilot($url)
+{
+
+    $useragent = stream_context_create(
+        array(
+            "http" => array(
+                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+            )
+        )
+    );
+
+    $page = file_get_contents($url, false, $useragent);
+    $split_data = "data-rating-typography=\"true\">";
+    $parsed = get_string_between($page, $split_data, "</p>");
+    $floatvalue = floatval($parsed);
+
+    return $floatvalue;
+}
+
+
+function get_total_review($url)
+{
+
+    $useragent = stream_context_create(
+        array(
+            "http" => array(
+                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+            )
+        )
+    );
+
+    $page = file_get_contents($url, false, $useragent);
+    $split_data = "typography_fontstyle-normal__kHyN3 styles_text__W4hWi";
+    $parsed = get_string_between($page, $split_data, "<!-- -->");
+    $floatvalue = floatval($parsed);
+
+    return $floatvalue;
+}
+
+
 
 function get_string_between($string, $start, $end)
 {
@@ -9,5 +50,4 @@ function get_string_between($string, $start, $end)
     $len = strpos($string, $end, $ini) - $ini;
     return substr($string, $ini, $len);
 }
-
-?> 
+?>
--- index.php
+++ index.php
@@ -1,28 +1,36 @@
-<?php 
-include("functions.php"); 
+<?php
+
+
+include("functions.php");
 
 
 
-// WEB SCRAPPING STUFFg
-$useragent = stream_context_create(
-	array(
-	    "http" => array(
-		"header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
-	    )
-	)
-    );
-    
+$floatvalue = "";
+$totalreview = "";
 
-$url = "https://www.trustpilot.com/review/digitstudio.fr"; 
-$page = file_get_contents($url, false, $useragent);
-$split_data = "data-rating-typography=\"true\">"; 
-$parsed = get_string_between($page, $split_data,"</p>");
-$floatvalue = floatval($parsed);
+if (isset($_GET['url'])) {
 
+	$floatvalue = get_score_from_trustpilot("https://www.trustpilot.com/review/digitstudio.fr"); // GET GLOBAL SCORE FROM TRUSTPILOT OF OUR WEBSITE like for example 2.5/5 
+	if (isset($_GET['score'])) {
+		echo $floatvalue;
+	}
 
-// FINAL VALUE RETURNED 
-$img_stars_trustpilot = "https://cdn.trustpilot.net/brand-assets/4.1.0/stars/stars-".$floatvalue.".svg";
-echo $img_stars_trustpilot;
-echo "<br>";
-echo $floatvalue;
-?> 
+	if (isset($_GET['scoreimg'])) {
+		echo "<img src='img/stars-" . $floatvalue . ".svg'</img>";
+	}
+
+	if (isset($_GET['totalreview'])) {
+		$totalreview = get_total_review("https://www.trustpilot.com/review/digitstudio.fr");
+		echo $totalreview;
+	}
+
+	if (isset($_GET['widgetmini'])) {
+		if ($totalreview == "") {
+			$totalreview = get_total_review("https://www.trustpilot.com/review/digitstudio.fr");
+			include("widgetmini.php"); 
+		}
+	}
+} else {
+	echo "URL is a parameter needed";
+	exit;
+}
